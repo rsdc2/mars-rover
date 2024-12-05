6af9a3e212a0e44d95d0efc13893928b720bb26c
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-05 10:00:08 +0000

    Add project files.

---
+++ MarsRover.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+
+namespace MarsRover.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ MarsRover/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MarsRover.Tests")]
+
+namespace MarsRover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
