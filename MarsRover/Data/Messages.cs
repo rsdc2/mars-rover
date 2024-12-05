5ef781cc99d706ed14fcb5f4ca6364e48c8534b2
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-05 17:10:46 +0000

    Add Rover messages

--- MarsRover/Data/Messages.cs
+++ MarsRover/Data/Messages.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MarsRover.Model;
 
 namespace MarsRover.Data;
 
+
 internal static class Messages
 {
+    public static string CannotAddPlateau = "Cannot add plateau";
+    public static string CannotAddRover = "Cannot add Rover";
     public static string CommandsNotCarriedOut = "Commands not carried out";
     public static string EmptyInput = "No input received";
     public static string ParseFailure = "could not be parsed";
     public static string NoInstruction = "No instruction given";
     public static string NoPosition = "No position given";
 
+    public static string CannotRotateRover(Rover rover)
+    {
+        return $"Cannot rotate Rover {rover.Id}";
+    }
+
+    public static string CannotRotateRover(int roverId)
+    {
+        return $"Cannot rotate Rover {roverId}";
+    }
 
     public static string InvalidDirection(char direction)
     {
