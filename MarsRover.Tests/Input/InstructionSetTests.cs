c665b8798247868d427760f36afc17625b6479d7
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-05 14:16:29 +0000

    add iterable InstructionSet

---
+++ MarsRover.Tests/Input/InstructionSetTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MarsRover.Input;
+using MarsRover.Types;
+using MarsRover.Data;
+
+namespace MarsRover.Tests.Input;
+
+internal class InstructionSetTests
+{
+    [Test]
+    public void CreateInstructionSet()
+    {
+        // Arrange
+        var instructions = new InstructionSet([Instruction.L, Instruction.R]);
+
+        // Act
+
+        // Assert
+        Assert.That(instructions.Count() == 2);
+    }
+
+    [Test]
+    public void CanIterateThroughInstructionSetTest()
+    {
+        // Arrange
+        var instructions = new InstructionSet([Instruction.L, Instruction.R]);
+        List<Instruction> instructionList = [];
+
+        // Act
+        foreach (var instruction in instructions)
+        {
+            instructionList.Add(instruction);
+        }
+
+        // Assert
+        Assert.That(instructionList.Count == 2);
+    }
+
+}
---
+++ MarsRover/Input/InstructionSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MarsRover.Data;
+using MarsRover.Types;
+
+namespace MarsRover.Input
+{
+    internal class InstructionSet : IEnumerable<Instruction>
+    {
+        private List<Instruction> Instructions { get; set; }
+
+        public InstructionSet(List<Instruction> instructions) 
+        {
+            Instructions = instructions;
+        }
+
+        public static InstructionSet FromList(List<Instruction> instructions)
+        {
+            return new InstructionSet(instructions);    
+        }
+
+        public IEnumerator<Instruction> GetEnumerator()
+        {
+            return ((IEnumerable<Instruction>)Instructions).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)Instructions).GetEnumerator();
+        }
+
+    }
+}
