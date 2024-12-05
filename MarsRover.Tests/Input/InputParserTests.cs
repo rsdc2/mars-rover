68858be433d3543379e815abc075f68590e832e0
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-05 14:16:08 +0000

    add InputParser to parse input with ParseInstruction method

---
+++ MarsRover.Tests/Input/InputParserTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MarsRover.Input;
+using MarsRover.Types;
+using MarsRover.Data;
+
+namespace MarsRover.Tests.Input;
+
+internal class InputParserTests
+{
+    [Test]
+    public void InvalidInputReturnsFailure()
+    {
+        // Arrange
+        string instruction = "";
+
+        // Act
+        var result = InputParser.ParseInstruction(instruction);
+
+        // Assert
+        Assert.That(result.Message == Messages.NoInstruction);
+    }
+
+    [Test]
+    public void InvalidInstructionsReturnsMessageWithFailureDescriptions()
+    {
+        // Arrange
+        string instructions = "TDR";
+
+        // Act
+        var result = InputParser.ParseInstruction(instructions);
+
+        // Assert
+        result.Value.Message.Should().Be($"{Messages.CommandsNotCarriedOut}" +
+            $":\nT {Messages.ParseFailure}\nD {Messages.ParseFailure}");
+    }
+
+    [Test]
+    public void ValidInputReturnsSuccess()
+    {
+        // Arrange
+        string instruction = "MLR";
+
+        // Act
+        var result = InputParser.ParseInstruction(instruction);
+
+        // Assert
+        Assert.That(result.Value is Success<InstructionSet>);
+    }
+}
---
+++ MarsRover/Input/InputParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using MarsRover.Data;
+using MarsRover.Types;
+using MarsRover.Extensions;
+
+namespace MarsRover.Input;
+
+internal static partial class InputParser
+{
+    public static bool IsValidInstruction(string instruction)
+    {
+        var regex = InstructionRegex();
+        return regex.IsMatch(instruction);
+    }
+
+    public static Either<InstructionSet> ParseInstruction(string instructionString)
+    {
+
+        if (instructionString == String.Empty)
+        {
+            return Either<InstructionSet>.From(Messages.NoInstruction);
+        }
+
+        var instructions = instructionString.Select(c => c.ToInstruction());
+        var unwrapped = Either<Instruction>.Unwrap(instructions);
+        if (Either<Instruction>.Succeeded(instructions)) {
+            return unwrapped.Fmap(InstructionSet.FromList);
+        }
+
+        var failureMessages = unwrapped.Message;
+
+        return Either<InstructionSet>.From($"{Messages.CommandsNotCarriedOut}:\n{failureMessages}");
+    }
+
+    //public static Either<Position> ParsePosition(string position)
+    //{
+
+    //}
+
+    [GeneratedRegex(@"[LRM]+")]
+    private static partial Regex InstructionRegex();
+
+    [GeneratedRegex(@"^[0-5] [0-5]$")]
+    private static partial Regex PlateauSizeRegex();
+
+    [GeneratedRegex(@"^[0-5] [0-5] [NESW]$")]
+    private static partial Regex PositionRegex();
+}
