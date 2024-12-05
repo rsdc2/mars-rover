0b74def62e85910d83052a3fce2e3fc71c444c42
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-05 14:18:10 +0000

    Add Messages static class to carry messages to the user

---
+++ MarsRover/Data/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Data;
+
+internal static class Messages
+{
+    public static Dictionary<Failures, string> MessagesDict = new()
+    {
+        {Failures.CommandsNotCarriedOut, CommandsNotCarriedOut},
+        {Failures.ParseFailure, ParseFailure}
+    };
+
+    public static string CommandsNotCarriedOut = "Commands not carried out";
+    public static string ParseFailure = "could not be parsed";
+    public static string NoInstruction = "No instruction given";
+}
