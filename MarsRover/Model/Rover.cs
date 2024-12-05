bfd2ba57cfd6eb37137e932c3839d5d36cb41ee1
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-05 10:14:51 +0000

    Add domain types

---
+++ MarsRover/Model/Rover.cs
@@ -0,0 +1,16 @@
+﻿using MarsRover.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Model
+{
+    internal class Rover
+    {
+        public Position position { get; private set; }
+        public Rover() { }  
+
+    }
+}
