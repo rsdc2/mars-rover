1909c013a900c773f32a8069ece7b4a003ed1f85
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-05 14:15:03 +0000

    add extension method for char to convert to an Instruction

---
+++ MarsRover/Extensions/CharExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MarsRover.Data;
+using MarsRover.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Extensions;
+
+internal static class CharExtensions
+{
+    internal static Either<Instruction> ToInstruction(this char c) => c switch
+    {
+        'M' => Either<Instruction>.From(Instruction.M),
+        'R' => Either<Instruction>.From(Instruction.R),
+        'L' => Either<Instruction>.From(Instruction.L),
+        _ => Either<Instruction>.From($"{c} {Messages.ParseFailure}")
+    };
+}
