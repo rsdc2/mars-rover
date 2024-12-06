1d65f91f6a3d98c6e8cf04dbed1d9ac3bec27f47
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-06 14:14:33 +0000

    Create String extension methods for parsing to integer and to Direction

---
+++ MarsRover.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using MarsRover.Extensions;
+using MarsRover.Types;
+using MarsRover.Data;
+
+namespace MarsRover.Tests.Extensions
+{
+    internal class StringExtensionsTests
+    {
+        [Test]
+        [TestCase("123", 123)]
+        [TestCase("235", 235)]
+
+        public void StringToIntHappyTest(string input, int expectedOutput)
+        {
+            // Act
+            Either<int> output = input.ToInt();
+
+            // Assert
+            output.Result.Should().Be(expectedOutput);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("hello")]
+        [TestCase(".sdkjtw3")]
+
+        public void StringToIntSadTest(string input)
+        {
+            // Act
+            Either<int> output = input.ToInt();
+
+            // Assert
+            output.IsFailure.Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase("N", Direction.N)]
+        [TestCase("E", Direction.E)]
+        [TestCase("S", Direction.S)]
+        [TestCase("W", Direction.W)]
+        public void StringToDirectionHappyTest(string input, Direction expectedOutput)
+        {
+            // Act
+            Either<Direction> output = input.ToDirection();
+
+            // Assert
+            output.Result.Should().Be(expectedOutput);
+        }
+
+        [Test]
+        [TestCase("L")]
+        [TestCase("T")]
+        [TestCase(".sdkjtw3")]
+        public void StringToDirectionSadTest(string input)
+        {
+            // Act
+            Either<Direction> output = input.ToDirection();
+
+            // Assert
+            output.IsFailure.Should().BeTrue();
+        }
+    }
+}
---
+++ MarsRover/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using MarsRover.Data;
+using MarsRover.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Extensions;
+
+internal static class StringExtensions
+{
+    internal static Either<int> ToInt(this string s)
+    {
+        try
+        {
+            return Either<int>.From(int.Parse(s));
+        }
+        catch (Exception ex)
+        {
+            return Either<int>.From(Messages.CannotParseStringToInteger(s, ex.Message));
+        }
+    }
+
+    internal static Either<Direction> ToDirection(this string s) 
+    {
+        if (s.Length > 1) return Either<Direction>.From(Messages.InvalidDirection(s));    
+        if (s.Length == 0) return Either<Direction>.From(Messages.InvalidDirection(s));
+
+        return s[0].ToDirection();
+    }
+}
