37d856052141c4360d44079c4e605c2cb25000ba
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-21 20:55:39 +0000

    Use top level statements

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,12 +1,5 @@
-﻿namespace MarsRover;
-using MarsRover.UI;
+﻿using MarsRover.UI;
 
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        var mc__ = from mc in ConsoleUI.GetInitialSetup()
-                   from mc_ in ConsoleUI.HandleUserInstructions(mc, mc.Description())
-                   select mc_;
-    }
-}
+var mc__ = from mc in ConsoleUI.GetInitialSetup()
+            from mc_ in ConsoleUI.HandleUserInstructions(mc, mc.Description())
+            select mc_;
